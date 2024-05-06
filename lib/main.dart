46fd2a4cc4bed02af5de92cd80ffb9e064248eab
Ahmed Samir <ahmedsami.r@yahoo.com>
2024-05-06 21:58:16 +0300

    link app with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:open_market/core/router/app_router.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:open_market/firebase_options.dart';
+void main() async{
+   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const OpenMarket());
 }
 
