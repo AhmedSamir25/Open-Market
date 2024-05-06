cf1af81a52eed05671d5ac43e747da83b406497e
Ahmed Samir <ahmedsami.r@yahoo.com>
2024-05-06 22:16:44 +0300

    auth cubit and auth state

---
+++ lib/features/auth/logic/auth_cubit/auth_cubit.dart
@@ -0,0 +1,54 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:open_market/features/auth/data/repo/auth_repo.dart';
+
+part 'auth_state.dart';
+
+
+class AuthCubit extends Cubit<AuthState> {
+  final AuthRepository _authRepository;
+
+  AuthCubit(this._authRepository) : super(AuthInitial());
+
+  void registerWithEmail({
+    required String email,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      await _authRepository.registerWithEmail(email, password, name);
+      emit(UserSignSuccessState());
+    } catch (e) {
+      emit(FeiledCreatedUserState(massage: e.toString()));
+    }
+  }
+
+  void signIn({required String email, required String password}) async {
+    try {
+      await _authRepository.signIn(email, password);
+      emit(UserSignSuccessState());
+    } catch (e) {
+      emit(FeiledCreatedUserState(massage: e.toString()));
+    }
+  }
+
+  void signInWithGoogle() async {
+    try {
+      await _authRepository.signInWithGoogle();
+      emit(UserSignSuccessState());
+    } catch (e) {
+      emit(FeiledCreatedUserState(massage: e.toString()));
+    }
+  }
+  void signOut() async{
+    await _authRepository.signOut();
+  }
+
+  Future<void> resetPassword({required String email}) async {
+    try {
+      await _authRepository.resetPassword(email);
+      emit(SuccessResetPasswordState());
+    } catch (e) {
+      emit(FeiledResetPasswordState(massage: e.toString()));
+    }
+  }
+}
